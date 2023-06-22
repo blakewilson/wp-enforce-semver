3e15380cdf3d916ad241ae743f56b3032bf863bc
Blake Wilson <blake@blake.id>
2023-06-22 00:11:58 -0500

    Escape output

--- semantic-versioning.php
+++ semantic-versioning.php
@@ -125,13 +125,14 @@ function plugins_list_show_breaking_changes_message() {
 		$action_name .= $plugin_file_name;
 
 		$notice_text = __( '<br><br><b>THIS UPDATE MAY CONTAIN BREAKING CHANGES:</b> This plugin uses Semantic Versioning, and this new version is a major release. Please review the changelog before updating. <a href="https://semver.org" target="_blank">Learn more</a>', 'semantic-versioning' );
+		
 
 		add_action(
 			$action_name,
-			function( $data, $response ) use ($notice_text, $plugin_file_name) {
-				$notice_text = apply_filters('semantic_versioning_notice_text', $notice_text, $plugin_file_name, $data, $response);
+			function( $data, $response ) use ( $notice_text, $plugin_file_name ) {
+				$notice_text = apply_filters( 'semantic_versioning_notice_text', $notice_text, $plugin_file_name, $data, $response );
 
-				printf( $notice_text );
+				printf( wp_kses_post($notice_text) );
 			},
 			10,
 			2
