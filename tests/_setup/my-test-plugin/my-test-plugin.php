3cfd4b82a236d947a52dbddf2ab3c598f0a483d8
Blake Wilson <blake@blake.id>
2024-06-29 17:39:42 -0500

    Get tests setup, working on mock plugin update

--- tests/_setup/my-test-plugin/my-test-plugin.php
+++ tests/_setup/my-test-plugin/my-test-plugin.php
@@ -13,3 +13,27 @@ Version: 1.0.0
 // use EnforceSemVer\EnforceSemVer;
 // 
 // new EnforceSemVer('my-test-plugin/my-test-plugin.php');
+
+
+add_filter('site_transient_update_plugins', 'mock_plugin_update');
+
+function mock_plugin_update($transient) {
+    if (empty($transient->checked)) {
+        return $transient;
+    }
+
+    $plugin_file = 'my-test-plugin/my-test-plugin.php';
+
+    $new_version = '2.0.0';
+
+    $update_data = (object) [
+        'slug'        => 'my-test-plugin',
+        'new_version' => $new_version,
+        'url'         => 'https://example.com/my-custom-plugin-changelog', // URL to the plugin changelog
+        'package'     => 'https://example.com/my-custom-plugin-v2.0.0.zip', // URL to the new version zip file
+    ];
+
+    $transient->response[$plugin_file] = $update_data;
+
+    return $transient;
+}
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('test env is running', async ({ page }) => {
+  await page.goto('http://localhost:8888')
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  expect(true).toBe(true)
+})
+
+test('has test plugin installed', async ({ page }) => {
+  await page.goto('http://localhost:8888/wp-admin/plugins.php');
+
+  await expect(page.getByRole('row').nth(2)).toContainText('My Test Plugin');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+// 
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+// 
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
